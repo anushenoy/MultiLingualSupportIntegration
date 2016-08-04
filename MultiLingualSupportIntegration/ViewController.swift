f021b4da5a0ed8402dac5adcf49513de03ee8615
Admin <admin@pranays-macbook-pro.local>
2016-08-04 23:06:02 +0530

    Initial Commit

---
+++ MultiLingualSupportIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiLingualSupportIntegration
+//
+//  Created by Admin on 04/08/16.
+//  Copyright © 2016 Anush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
